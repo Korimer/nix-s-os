e869e8cf2be03b8d26dc31ffecfd4109c01a2ed6
Korimer <thekclov@gmail.com>
2025-11-07 10:52:52 -0700

    finally removed vestigial scripts; have a clean programs script

--- programs.nix
+++ programs.nix
@@ -1,20 +1,24 @@
-{lib, pkgs, ...}: {
-  # Builtins
-  programs.firefox.enable = true;
-  programs.git.enable = true;
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
+{lib, pkgs, ...}: 
+let
+  #Builtins with enable
+  sys_programs_to_enable = {
+    firefox = {};
+    git = {};
+    thunderbird = {};
+    neovim = {
+      defaultEditor = true;
+    };
   };
-  programs.thunderbird.enable = true;
-
-  programs.ssh = {
-    startAgent = true;
-    enableAskPassword = true;
+  # Builtins with toggleable settings
+  sys_programs_config = {
+    ssh = {
+      startAgent = true;
+      enableAskPassword = true;
+    };
   };
-
-  environment.systemPackages = with pkgs; [
+  
+  #Installable packages
+  sys_pkgs = with pkgs; [
     ghostty
     remmina
     slack
@@ -22,4 +26,24 @@
     xclip
     git
   ];
+  
+  ###########
+  ## Logic ##
+  ###########
+  sys_programs_enabled = lib.mapAttrs (
+      name: value:
+      value // {enable = true;}
+    )
+    (sys_programs_to_enable)
+  ;
+  
+  sys_programs_complete = lib.recursiveUpdate 
+    sys_programs_enabled
+    sys_programs_config
+  ;
+  
+in { # yeah the process of actually setting it all is 2 lines lol
+  programs = sys_programs_complete;
+  environment.systemPackages = sys_pkgs;
 }
+
