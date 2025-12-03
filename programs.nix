fddf04c9f4fa8ccb8407dec1dedd7689f1a36eea
Korimer <thekclov@gmail.com>
2025-12-03 12:11:16 -0700

    barely working agenix; going to try sops

--- programs.nix
+++ programs.nix
@@ -1,4 +1,4 @@
-{lib, pkgs, ...}: 
+{lib, pkgs, agenixTar, ...}: 
 let
   #Builtins with enable
   sys_programs_to_enable = {
@@ -20,17 +20,18 @@ let
   };
   # Builtins with toggleable settings
   sys_programs_config = {
-    ssh = {
-      startAgent = true;
-      enableAskPassword = true;
-    };
+#    ssh = {
+#      startAgent = true;
+#      enableAskPassword = true;
+#    };
   };
   
   #Installable packages
   sys_pkgs = with pkgs; [
+    (pkgs.callPackage "${agenixTar}/pkgs/agenix.nix" {})
+ #   discord
     xorg.xkill
     wireshark
-    ghostty
     remmina
     slack
     neovide
