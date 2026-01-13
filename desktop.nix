c857caaea7a70cd7782cdd5043780ae74c514b82
Korimer <thekclov@gmail.com>
2026-01-13 10:37:17 -0700

    slight polish on desktop

--- desktop.nix
+++ desktop.nix
@@ -1,35 +1,15 @@
-{pkgs, ...}: let
-  #flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
-  #hyprland = (import flake-compat {
-  #  src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/main.tar.gz";
-  #}).defaultNix;
-in {
+{pkgs, ...}: {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
-    xwayland.enable = true;
-    # set the flake package
-  #  package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-  #  portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    # Would be nice to have this, but it's broken as of https://github.com/hyprwm/Hyprland/issues/9750
+    xwayland.enable = false; 
   };
+  # Oh well...
   services.xserver.enable = true;
   
-  #nix.settings = {
-  #  substituters = ["https://hyprland.cachix.org"];
-  #  trusted-substituters = ["https://hyprland.cachix.org"];
-  #  trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  #};
-
-  ## Enable the X11 windowing system.
-  #services.xserver.enable = true;
-  #services.displayManager.gdm.wayland = true;
-  #programs.hyprland = {
-  #  enable = true  #  withUWSM = true;
-  #  xwayland.enable = true;
-  #};
   environment.systemPackages = with pkgs; [
+    hyprshot # Screenshots
     socat # System util for cross-app communication
     ddcutil # Brightness
     swaylock-effects # Lockscreen
@@ -56,6 +36,7 @@ in {
       };
     };
   };
+
   #services.xserver.videoDrivers = ["nvidia"];
   #hardware.graphics.enable = true;
   #hardware.nvidia = {
@@ -64,14 +45,7 @@ in {
   #    enable = true;
   #  };
   #};
-
-  #programs.hyprlock.enable = true;
-  #services.hypridle.enable = true;
   
-  # Enable the GNOME Desktop Environment.
-  #services.displayManager.gdm.enable = true;
-  #services.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
