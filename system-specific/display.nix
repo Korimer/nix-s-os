586dad739dd7a61dedf5b81989073366a1714960
Korimer <thekclov@gmail.com>
2026-03-03 10:52:07 -0700

    killed everythinga gani

--- system-specific/display.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-  
-  systemd.tmpfiles.rules = [
-    "L /run/gdm/.config/monitors.xml etc/nixos/system-specific/monitors.xml"
-  ];
-}
--- system-specific/non-config/secrets.nix
+++
@@ -1,6 +0,0 @@
-let 
-  ets-c837275181 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJepOmvg/hyQ5f0ol4YsVCuTfr5GinN9dnuIavluccv";
-in
-{
-  "secret1.age".publicKeys = [ ets-c837275181 ];
-}
