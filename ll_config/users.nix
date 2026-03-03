9c8cd6e6146e196e7f3153ab1a7f34506fe5552c
Korimer <thekclov@gmail.com>
2026-03-03 11:01:00 -0700

    unscrewed modules partiall

--- ll_config/users.nix
+++
@@ -1,8 +0,0 @@
-{...}: {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.k = {
-    isNormalUser = true;
-    description = "k";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-}
--- system-specific/core.nix
+++
@@ -1,6 +0,0 @@
-{...}: {
-  imports = [
-    ./display.nix
-    ./misc.nix
-  ];
-}
