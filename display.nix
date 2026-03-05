028575c7994968c1882fef70c7ab9b0fb555fdef
= <=>
2026-03-05 10:59:19 -0700

    display is unecessary with the new system

--- display.nix
+++ display.nix
@@ -1,6 +1,6 @@
 {...}: {
   
-  systemd.tmpfiles.rules = [
-    "L /run/gdm/.config/monitors.xml etc/nixos/system-specific/monitors.xml"
-  ];
+  #systemd.tmpfiles.rules = [
+  #  "L /run/gdm/.config/monitors.xml etc/nixos/system-specific/monitors.xml"
+  #];
 }
