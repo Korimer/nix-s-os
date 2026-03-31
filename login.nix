8d468e32168e180d1883d103e449fc4983f127cc
= <=>
2026-03-31 17:19:25 -0600

    reverted back to sddm, playing with login once more

--- login.nix
+++ login.nix
@@ -1,17 +1,22 @@
 {pkgs, ...}: 
-{ 
-  services.xserver = {
-    enable = true;
-    displayManager.lightdm = {
+let
+  custom_astro = pkgs.sddm-astronaut.override { embeddedTheme = "black_hole"; };
+  chosen_theme = 
+    "${custom_astro}/share/sddm/themes/sddm-astronaut-theme";
+in {
+  services.displayManager = {
+    autoLogin = {
+      enable = false;
+      user = null;
+    };
+    sddm = {
       enable = true;
-      greeters.gtk = {
-        enable = true;
-        extraConfig = ''
-          [greeter]
-          background=/etc/nixos/fauux_bg.gif
-        '';
-      };
+      wayland.enable = true;
+      theme = chosen_theme;
+      extraPackages = [
+        pkgs.kdePackages.qt5compat
+        custom_astro
+      ];
     };
   };
 }
-
