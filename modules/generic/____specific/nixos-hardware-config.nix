8f54c23a4790f633720e36057a9a403e35c3fa87
Korimer <thekclov@gmail.com>
2026-04-22 13:07:01 -0600

    Revert "moving things into place"
    This reverts commit 11a19b423fddbdbf6d6e103a2e5736437cb5069d.

--- modules/generic/____specific/nixos-hardware-config.nix
+++
@@ -1,6 +0,0 @@
-{...}:
-{
-  imports = [
-    "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/asus/fa507nv"
-  ]; 
-}
--- modules/generic/desktop/wleave.nix
+++
@@ -1,7 +0,0 @@
-{config, ...}: {
-
-  autoMkLink.targets.
-    "${config.environment.variables.NIXROOT}/static/desktop/wleave"
-    = "/etc/wleave"
-  ;
-}
