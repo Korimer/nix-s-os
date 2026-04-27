8278f6d185bb8e3fb25ede900c1bff1e2552b5bf
Korimer <thekclov@gmail.com>
2026-04-27 15:20:41 -0600

    working den.nix

--- modules/den.nix
+++ modules/den.nix
@@ -1,18 +1,59 @@
-{ inputs, den, ... }:
+{ den, inputs, ... }:
 {
+  # we can import this flakeModule even if we dont have flake-parts as input!
   imports = [ inputs.den.flakeModule ];
 
-  den.hosts.x86_64-linux.magic.users.korimer = { };
-
-  den.aspects.magic = {
-    nixos = { pkgs, ... }: {
-      environment.systemPackages = [ pkgs.hello ];
-      boot.loader.grub.enable = false; 
-      fileSystems."/".device = "/dev/null";
-    };
+  den.default.nixos = {
+    # remove for real host
+    fileSystems."/".device = "/dev/fake";
+    fileSystems."/".fsType = "auto";
+    boot.loader.grub.enable = false;
   };
 
-  den.aspects.korimer = {
-    includes = [ den.provides.primary-user ];
+  # tux user on igloo host, using nix-maid
+  den.hosts.x86_64-linux.igloo.users = {
+    korimer.classes = [];
+  };
+
+  # first: `npins add -n darwin github nix-darwin nix-darwin`
+  # den.hosts.aarch64-darwin.apple.users.tux.classes = [ "hjem" ];
+
+  # host aspect
+  den.aspects.igloo = {
+    nixos =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [
+          pkgs.vim
+        ];
+      };
+  };
+
+  # include den batteries or your own re-usable aspects
+  # this affects all users, could also be done per user
+  den.ctx.user.includes = [ den.provides.define-user ];
+
+  # user aspect
+  den.aspects.tux = {
+    # user configures host <nixos/darwin>.users.users.tux.description
+    # Read docs about the `user` class.
+    user.description = "Cute Penguin";
+
+    # user contributes nixos and darwin common config
+    os =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [ pkgs.hello ];
+      };
+
+    # maid class
+    maid.file.home.".gitconfig".text = ''
+      [user]
+        name=Tux
+    '';
+  };
+
+  den.aspects.pingu = {
+    hjem.files.".foo".text = "bar";
   };
 }
