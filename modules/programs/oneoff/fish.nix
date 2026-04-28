fc1be8ccb2491114497bfed7c436ac5902a45d02
Korimer <thekclov@gmail.com>
2026-04-28 13:43:03 -0600

    reimplemented update alias

--- modules/programs/oneoff/fish.nix
+++ modules/programs/oneoff/fish.nix
@@ -1,6 +1,13 @@
 { ... }:
 {
   den.aspects.fish = {
+    includes = [
+      ({ host, ... }: {
+       nixos = { pkgs, ... }: {
+       programs.fish.shellAliases.update = "${pkgs.nh}/bin/nh os switch --file /etc/nixos/ nixosConfigurations.${host.name}";
+       };
+       })
+    ];
     nixos = { pkgs, ... }:
     {
       programs.fish.enable = true;
