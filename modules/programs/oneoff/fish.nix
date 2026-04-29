a44c2ebb283d59588f44c2a195a28341135de43b
Korimer <thekclov@gmail.com>
2026-04-29 15:18:39 -0600

    nvm nh doesnt like sudo

--- modules/programs/oneoff/fish.nix
+++ modules/programs/oneoff/fish.nix
@@ -4,7 +4,7 @@
     includes = [
       ({ host, ... }: {
        nixos = { pkgs, ... }: {
-       programs.fish.shellAliases.update = "sudo ${pkgs.nh}/bin/nh os switch --file /etc/nixos/ nixosConfigurations.${host.name}";
+       programs.fish.shellAliases.update = "${pkgs.nh}/bin/nh os switch --file /etc/nixos/ nixosConfigurations.${host.name}";
        };
        })
     ];
