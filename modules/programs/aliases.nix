777ba47d59a13603c00261ae8a9c68650a44fcd8
Korimer <thekclov@gmail.com>
2026-05-01 16:23:54 -0600

    oop remove redundant programs

--- modules/programs/aliases.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  den.aspects.shellAliases.provides = {
-    nhUpdate = { host, ... }: {
-      nixos = { pkgs, ... }: { environment.shellAliases.update = "${pkgs.nh}/bin/nh os switch --file /etc/nixos/ nixosConfigurations.${host.name}"; };
-    };
-  };
-}
