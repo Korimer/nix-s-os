a8a7e5675ce8d1adef1f96992c2e7bd71a1cc253
Korimer <thekclov@gmail.com>
2026-05-01 12:56:46 -0600

    added work pc entry

--- modules/hosts/work-pc.nix
+++ modules/hosts/work-pc.nix
@@ -1,4 +1,17 @@
-{ ... }:
+{ den, ... }:
 {
+  den.hosts.x86_64-linux.fortnite.users = {
+    korimer.classes = [];
+  };
 
+  den.aspects.fortnite = {
+    includes = [
+      #den.aspects.nvidia
+      den.aspects.shellAliases.provides.nhUpdate
+    ];
+    nixos =
+    #{ pkgs, ... }:
+    {
+    };
+  };
 }
