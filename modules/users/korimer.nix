947f8afed88ad5e6c35c586b5ca606ba98d6be18
Korimer <thekclov@gmail.com>
2026-05-04 15:43:46 -0600

    sync?

--- modules/users/korimer.nix
+++ modules/users/korimer.nix
@@ -1,23 +1,38 @@
 { korimer, inputs, den, lib, ... }:
 let
 # see https://den.oeiuwq.com/guides/custom-classes/#example-alias-a-class-into-the-target-root
-globalsClass = { class, aspect-chain }: den.provides.forward {
-  each = [ "nixos" "darwin" ];
-  fromClass = _: "os";
-  intoClass = lib.id;
-  intoPath = _: [ ]; # top-level
+globalsClass = { class, aspect-chain }:
+den.provides.forward {
+  each = lib.singleton class;
+  fromClass = _: "global";
+  intoClass = _: "provides";
+  intoPath = _: [ ];
   fromAspect = _: lib.head aspect-chain;
+  adaptArgs = { config, ... }: { public = config; };
 };
+
+personalsClass = { class, aspect-chain }:
+den.provides.forward {
+  each = lib.singleton class;
+  fromClass = _: "personal";
+  intoClass = _: "provides";
+  intoPath = _: [ ];
+  fromAspect = _: lib.head aspect-chain;
+  adaptArgs = { config, ... }: {
+    private = lib.mkMerge (lib.attrValues config);
+  };
+};
+
 in
-{ den, ... }:
 {
   imports = [
     (inputs.den.namespace "korimer" true)
   ];
   den.aspects.korimer = {
-    includes = [ korimer.everywhere globalsClass ];
-    nixos = { pkgs, ... }:
-    {
-    };
+    includes = [
+      korimer.everywhere
+      globalsClass
+      personalsClass
+    ];
   };
 }
