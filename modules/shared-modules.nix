3f35f1a1b839d622b5cb091dab7f591764ea0fd5
Korimer <thekclov@gmail.com>
2026-05-04 16:53:17 -0600

    currently broken; sync anway

--- modules/shared-modules.nix
+++ modules/shared-modules.nix
@@ -1,7 +1,41 @@
-{ den, korimer, ... }:
+{ den, korimer, lib, ... }:
+let
+# see https://den.oeiuwq.com/guides/custom-classes/#example-alias-a-class-into-the-target-root
+globalsClass = { class, aspect-chain }:
+den.provides.forward {
+  each = lib.singleton class;
+  fromClass = _: "global";
+  intoClass = _: "provides";
+  intoPath = _: [ ];
+  fromAspect = _: lib.head aspect-chain;
+  adaptArgs = { config, ... }: { public = config; };
+};
+
+personalsClass = { class, aspect-chain }:
+den.provides.forward {
+  each = lib.singleton class;
+  fromClass = _: "personal";
+  intoClass = _: "provides";
+  intoPath = _: [ ];
+  fromAspect = _: lib.head aspect-chain;
+  adaptArgs = { config, ... }: {
+    private = lib.mkMerge (lib.attrValues config);
+  };
+};
+
+in
 {
-  den.default.includes = [
-    korimer.everywhere
-    den.aspects.personalScripts.all
-  ];
+  den.default = {
+    includes = [
+      korimer.everywhere
+      #den.aspects.personalScripts.all
+      den.aspects.korimer.allProvides
+    ];
+
+    allProvides = {};
+
+    provides.allProvides = { class, aspect-chain }: {
+      includes = (lib.attrValues (class.provides));
+    };
+  };
 }
--- modules/users/korimer.nix
+++ modules/users/korimer.nix
@@ -1,29 +1,4 @@
 { korimer, inputs, den, lib, ... }:
-let
-# see https://den.oeiuwq.com/guides/custom-classes/#example-alias-a-class-into-the-target-root
-globalsClass = { class, aspect-chain }:
-den.provides.forward {
-  each = lib.singleton class;
-  fromClass = _: "global";
-  intoClass = _: "provides";
-  intoPath = _: [ ];
-  fromAspect = _: lib.head aspect-chain;
-  adaptArgs = { config, ... }: { public = config; };
-};
-
-personalsClass = { class, aspect-chain }:
-den.provides.forward {
-  each = lib.singleton class;
-  fromClass = _: "personal";
-  intoClass = _: "provides";
-  intoPath = _: [ ];
-  fromAspect = _: lib.head aspect-chain;
-  adaptArgs = { config, ... }: {
-    private = lib.mkMerge (lib.attrValues config);
-  };
-};
-
-in
 {
   imports = [
     (inputs.den.namespace "korimer" true)
@@ -31,8 +6,10 @@ in
   den.aspects.korimer = {
     includes = [
       korimer.everywhere
-      globalsClass
-      personalsClass
     ];
+  };
+
+  den.aspects.korimer.provides.nothing = {
+    nixos = {pkgs, ...}: { environment.systemPackages = [pkgs.etcd]; };
   };
 }
