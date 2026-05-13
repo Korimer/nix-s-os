776d4d3b1605eeae44872d2c77f20aa38048147b
Korimer <thekclov@gmail.com>
2026-05-13 17:48:54 -0600

    cleanup

--- modules/features/korimer/games.nix
+++ modules/features/korimer/games.nix
@@ -1,24 +1,11 @@
 { inputs, ... }:
 {
-
-
-  #flake-file.inputs.helium = {
-  #  url = "github:vikingnope/helium-browser-nix-flake";
-  #};
-
   den.aspects.korimer.provides.games = {
     nixos = { pkgs, ... }: {
       environment.systemPackages = [
         pkgs.everest
         (pkgs.olympus.override {celesteWrapper = "steam-run";})
       ];
-      #nixpkgs.overlays = [
-      #  inputs.helium.overlays.default
-      #];
-      #environment.systemPackages = [
-      #  pkgs.etcd
-      #  pkgs.helium
-      #];
     };
   };
 }
