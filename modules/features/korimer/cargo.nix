b55ac7525eb7574ca67c127b113e378e394621bb
Korimer <thekclov@gmail.com>
2026-05-19 01:22:22 -0600

    ok cool cargo/fenix is pinned now

--- modules/features/korimer/cargo.nix
+++ modules/features/korimer/cargo.nix
@@ -1,19 +1,17 @@
-{ ... }:
+{ inputs, ... }:
 {
   den.aspects.korimer.provides.cargo = {
     nixos = { pkgs, ... }: {
-      nixpkgs.overlays = [
-        (import "${fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"}/overlay.nix")
-      ];
-      environment.systemPackages = with pkgs; [
-        (fenix.complete.withComponents [
+      nixpkgs.overlays = [ inputs.fenix.overlays.default ];
+      environment.systemPackages = [
+        (pkgs.fenix.complete.withComponents [
          "cargo"
          "clippy"
          "rust-src"
          "rustc"
          "rustfmt"
         ])
-          rust-analyzer-nightly
+          pkgs.rust-analyzer-nightly
       ];
     };
   };
