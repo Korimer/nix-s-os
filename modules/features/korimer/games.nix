ec3f605a7743e48b66c1cd3f55e9a6b248123a47
Korimer <thekclov@gmail.com>
2026-05-20 15:02:26 -0600

    added sober to games

--- modules/features/korimer/games.nix
+++ modules/features/korimer/games.nix
@@ -2,10 +2,20 @@
 {
   den.aspects.korimer.provides.games = {
     nixos = { pkgs, ... }: {
-      environment.systemPackages = [
-        pkgs.everest
-        (pkgs.olympus.override {celesteWrapper = "steam-run";})
+      environment.systemPackages = with pkgs; [
+        everest
+          (olympus.override {celesteWrapper = "steam-run";})
       ];
+
+      services.flatpak.enable = true;
+      systemd.services.flatpak-repo = {
+        wantedBy = [ "multi-user.target" ];
+        path = [ pkgs.flatpak ];
+        script =
+          ''
+          flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+          '';
+      }; 
     };
   };
 }
