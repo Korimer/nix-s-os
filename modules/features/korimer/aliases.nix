8359694badf39984b79cbe0108da6b68dc7f955a
Korimer <thekclov@gmail.com>
2026-05-21 16:07:45 -0600

    fixed/cleaned up aliases

--- modules/features/korimer/aliases.nix
+++ modules/features/korimer/aliases.nix
@@ -4,11 +4,19 @@
 
     includes = lib.attrValues den.aspects.korimer.provides.shellAliases.provides;
 
-    provides.nhUpdate = { host, ... }: {
-      nixos = { config, pkgs, ... }: { environment.shellAliases.update = "${pkgs.nh}/bin/nh os switch --file ${config.environment.variables.NIXROOT} nixosConfigurations.${host.name}"; };
-    };
-    provides.flakeGen = _: {
-        nixos = { config, ... }: { environment.shellAliases.WriteFlake = "cd ${config.environment.variables.NIXROOT} && nix run .#write-flake"; };
+    provides = {
+      nhUpdate = { host, ... }: {
+        nixos = { config, pkgs, ... }: {
+          environment.shellAliases.update =
+            "${pkgs.nh}/bin/nh os switch --file /etc/nixos nixosConfigurations.${host.name}";
+        };
       };
+      writeFlake = {
+        nixos = { config, ... }: {
+          environment.shellAliases.WriteFlake =
+            "(cd /etc/nixos && nix run .#write-flake)";
+        };
+      };
+    };
   };
 }
