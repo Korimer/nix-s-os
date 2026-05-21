e4f05af0dcaf6714db1776329a356c744f7ab6a8
Korimer <thekclov@gmail.com>
2026-05-21 14:19:50 -0600

    added flake.lock flattening

--- flake.nix
+++ flake.nix
@@ -11,12 +11,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    flatpak = {
-      type = "github";
-      owner = "gmodena";
-      repo = "nix-flatpak";
-      ref = "latest";
-    };
     helium.url = "github:vikingnope/helium-browser-nix-flake";
     import-tree.url = "github:vic/import-tree";
     nix-auto-follow = {
