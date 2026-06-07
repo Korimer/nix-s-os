7695303cbf3260e67a886c2d35d227580da6d5d5
Korimer <thekclov@gmail.com>
2026-06-07 15:39:12 -0600

    may have implemented a hacky means of grabbing flake root

--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -1,4 +1,10 @@
 { inputs, ... }:
+let
+  writeFlakeRoot = pkgs: pkgs.writeShellScriptBin "write-flake-root"
+  ''
+    echo "$PWD" > "$PWD/flake-root.nix"
+  '';
+in
 {
   imports = with inputs.flake-file.flakeModules; [
     dendritic
@@ -19,7 +25,13 @@
       den.url = "github:vic/den";
       import-tree.url = "github:vic/import-tree";
       self.submodules = true;
-    }; 
+    };
+    write-hooks = [
+      {
+        index = 101;
+        program = writeFlakeRoot;
+      }
+    ];
     prune-lock.enable = true;
   };
 }
