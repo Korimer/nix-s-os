f75346addabcb7f15e6985c4c92720f5385197d5
= <=>
2026-06-07 18:23:14 -0600

    remove own flake root

--- flake.nix
+++ flake.nix
@@ -18,12 +18,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    flake-root = {
-      type = "github";
-      owner = "srid";
-      repo = "flake-root";
-      rev = "b759a56851e10cb13f6b8e5698af7b59c44be26e";
-    };
     flatpak = {
       type = "github";
       owner = "gmodena";
