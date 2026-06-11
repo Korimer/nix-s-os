f6715495cd984e49d1b0f97fd7ae1b2e76b64859
Korimer <112779222+Korimer@users.noreply.github.com>
2026-06-11 12:08:09 -0600

    something resembling function

--- modules/features/korimer/waybar/waybar.nix
+++ modules/features/korimer/waybar/waybar.nix
@@ -1,105 +1,106 @@
 { flake-root, inputs, ... }:
-{
-  den.aspects.korimer.provides.waybar.homeManager = { config, ... }:
-  let
-  colorscheme = import "${inputs.self}/modules/_nondendric/colorschemes/powerline.nix";
-  colors = {
-    swaync      = colorscheme.yellow;
-    wireplumber = colorscheme.blue;
-    workspaces  = colorscheme.cyan;
-    tray        = colorscheme.purple;
-    window      = colorscheme.blue;
-    taskbar     = colorscheme.purple;
-    cpu         = colorscheme.yellow;
-    memory      = colorscheme.orange;
-    temperature = colorscheme.green;
-    clock       = colorscheme.orange;
-    battery     = colorscheme.yellow;
-    power       = colorscheme.magenta;
-  };
+let
+colorscheme = import "${inputs.self}/modules/_nondendric/colorschemes/powerline.nix";
+colors = {
+  swaync      = colorscheme.yellow;
+  wireplumber = colorscheme.blue;
+  workspaces  = colorscheme.cyan;
+  tray        = colorscheme.purple;
+  window      = colorscheme.blue;
+  taskbar     = colorscheme.purple;
+  cpu         = colorscheme.yellow;
+  memory      = colorscheme.orange;
+  temperature = colorscheme.green;
+  clock       = colorscheme.orange;
+  battery     = colorscheme.yellow;
+  power       = colorscheme.magenta;
+};
 
-  modules = {
-    left = [
-      "custom/swaync"
-        "custom/div-swaync-wireplumber"
-        "wireplumber"
-        "custom/div-wireplumber-workspaces"
-        "niri/workspaces"
-        "custom/div-workspaces-tray"
-        "tray"
-        "custom/div-tray-window"
-        "niri/window"
-        "custom/window-r-flair"
-    ];
-    center = [
-      "wlr/taskbar"
-    ];
-    right = [
-      "custom/cpu-l-flair"
-        "cpu"
-        "custom/div-cpu-memory"
-        "memory"
-        "custom/div-memory-temperature"
-        "temperature"
-        "custom/div-temperature-clock"
-        "clock"
-        "battery"
-        "custom/div-clock-power"
-        "custom/power"
-        "custom/power-r-flair"
-    ];
-  };
+modules = {
+  left = [
+    "custom/swaync"
+      "custom/div-swaync-wireplumber"
+      "wireplumber"
+      "custom/div-wireplumber-workspaces"
+      "niri/workspaces"
+      "custom/div-workspaces-tray"
+      "tray"
+      "custom/div-tray-window"
+      "niri/window"
+      "custom/window-r-flair"
+  ];
+  center = [
+    "wlr/taskbar"
+  ];
+  right = [
+    "custom/cpu-l-flair"
+      "cpu"
+      "custom/div-cpu-memory"
+      "memory"
+      "custom/div-memory-temperature"
+      "temperature"
+      "custom/div-temperature-clock"
+      "clock"
+      "battery"
+      "custom/div-clock-power"
+      "custom/power"
+      "custom/power-r-flair"
+  ];
+};
 
-  allmodules = modules.left ++ modules.right ++ modules.center;
+allmodules = modules.left ++ modules.right ++ modules.center;
 
-  colorOptions = builtins.concatStringsSep "|" (builtins.attrNames colors);
-  matchdiv = "(custom\/div-(${colorOptions})-(${colorOptions}))";
-  divModules = builtins.filter
-    (mod: mod != null)
-    (builtins.map
-      (modname: builtins.match matchdiv modname)
-      (allmodules)
-    );
-
-  divAttrs = builtins.listToAttrs 
-    (map (mod: {
-        name = builtins.head mod;
-        value = {
-          format = "";
-          tooltip = false;
-        };
-      })
-      divModules
-    );
-
-  divCSS = let elm = builtins.elemAt; in
-  builtins.concatStringsSep "\n" (
-    builtins.map
-      (list: ''
-        #${elm list 0} {
-          color: ${colors."${(elm list 1)}"};
-          background-color: ${colors."${(elm list 2)}"};
-        }
-      '')
-      divModules
+colorOptions = builtins.concatStringsSep "|" (builtins.attrNames colors);
+matchdiv = "(custom\/div-(${colorOptions})-(${colorOptions}))";
+divModules = builtins.filter
+  (mod: mod != null)
+  (builtins.map
+    (modname: builtins.match matchdiv modname)
+    (allmodules)
   );
-  in
+
+divAttrs = builtins.listToAttrs 
+  (map (mod: {
+      name = builtins.head mod;
+      value = {
+        format = "";
+        tooltip = false;
+      };
+    })
+    divModules
+  );
+
+divCSS = let elm = builtins.elemAt; in
+builtins.concatStringsSep "\n" (
+  builtins.map
+    (list: ''
+      #${builtins.replaceStrings ["/"] ["-"] (elm list 0)} {
+        color: ${colors."${(elm list 1)}"};
+        background-color: ${colors."${(elm list 2)}"};
+      }
+    '')
+    divModules
+);
+in
+{
+  den.aspects.korimer.provides.waybar.homeManager =
   {
+    home.file.tmp.text = divCSS;
     programs.waybar = {
-      settings = [{
+      settings = [(
+      {
         modules-left = modules.left;
         modules-center = modules.center;
         modules-right = modules.right;
       }
-      divAttrs
-      ];
+      // divAttrs
+      )];
       enable = true;
       systemd.enable = true;
       style = ''
         @import "${flake-root.literal}/modules/features/korimer/waybar/style.css";
       ''
-      + divCSS
-      ;
+      + divCSS;
     };
   };
 }
