1ec4b04a1a40a83c583f6d5a521abf6921b6050e
= <=>
2026-06-16 23:57:56 -0600

    configured raspi3 server

--- modules/hosts/home-server.nix
+++ modules/hosts/home-server.nix
@@ -1,4 +1,4 @@
-{ den, ... }:
+{ currentSystem, inputs, ... }:
 {
   flake-file.inputs.korimer-mini-server.url = "github:Korimer/MinimalNixServer";
   den.hosts.aarch64-linux.vorkuta.users = {
@@ -8,7 +8,19 @@
   den.aspects.vorkuta = {
     nixos = { pkgs, ... }:
     {
-      nixpkgs.buildPlatform = builtins.currentSystem;
+      imports = with inputs.korimer-mini-server.nixosModules; [
+        local-admin # aka asya <3
+        garbage-collector
+        firewall
+        fail2ban
+        ssh
+        nix-trust
+      ];
+      #Nix
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+      #Building
+      nixpkgs.buildPlatform = currentSystem;
       nixpkgs.hostPlatform = "aarch64-linux";
       system.stateVersion = "26.11";
 
@@ -24,17 +36,19 @@
       hardware.enableRedistributableFirmware = true;
 
       # networking
-      networking.wireless.enable = true;  
       networking.hostName = "vorkuta";
 
       time.timeZone = "America/Denver";
-      services.openssh.enable = true;
 
       security.sudo.wheelNeedsPassword = false;
 
+      environment.systemPackages = with pkgs; [ vim git htop ];
+
       # Boot
       boot.kernelPackages = pkgs.linuxPackages_latest;
-
+      boot.initrd.availableKernelModules = [ "usbhid" ];
+      boot.loader.grub.enable = false;
+      boot.loader.generic-extlinux-compatible.enable = true;
       boot.kernelParams = [
         "console=ttyS1,115200n8"
       ];
