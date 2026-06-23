b0a5338163c11b09c69c08f8be164169ffacb988
Korimer <thekclov@gmail.com>
2026-06-23 17:53:33 -0600

    zamn it was that simple

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -7,33 +7,17 @@
   den.default.includes = [ den.aspects.secrets ];
   den.aspects.secrets = {
     nixos = { pkgs, ... }: {
+      programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+        pinentryPackage = pkgs.pinentry-gtk2;
+      };
+
       services.gnome = {
-        gnome-keyring.enable = true;
-        gcr-ssh-agent.enable = true;
-        gnome-online-accounts.enable = lib.mkForce false;
+        gnome-keyring.enable = false;
+        gcr-ssh-agent.enable = false;
+        gnome-online-accounts.enable = false;
       };
-
-      environment.sessionVariables = {
-        SSH_AUTH_SOCK = "";
-      };
-
-      services.pcscd.enable = true;
-
-      # Prevent SSH from managing its own agent
-      programs.ssh.startAgent = false;
-      programs.ssh.enableAskPassword = false;
-
-      #services.openssh.settings.AddKeysToAgent = "yes";
-      
-      # GPG agent (GPG ONLY, NOT SSH)
-      #programs.gnupg.agent = {
-      #  enable = true;
-      #  enableSSHSupport = false;
-      #  pinentryPackage = pkgs.pinentry-qt;
-      #};
-      environment.systemPackages = with pkgs; [
-        #pinentry-qt
-      ];
     };
   };
 }
