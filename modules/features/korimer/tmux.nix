634f72df38e7ccf838c5956924c710506a9f36dd
Korimer <thekclov@gmail.com>
2026-06-23 22:21:59 -0600

    wip tmux

--- modules/features/korimer/tmux.nix
+++ modules/features/korimer/tmux.nix
@@ -5,5 +5,18 @@
         enable = true;
       };
     };
+
+    homeManager = { lib, ... }: {
+      programs.tmux.extraConfig =
+        lib.mkAfter "source-file ~/.config/tmux/tmux.conf";
+    };
+
+    provides.shellInit = {
+      nixos = { lib, ... }: {
+        environment.shellInit = lib.mkAfter ''
+          
+        '';
+      };
+    };
   };
 }
