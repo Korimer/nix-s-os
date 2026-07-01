293a2790887882bf0c75e8a00eb5acea1395d5e8
Korimer <thekclov@gmail.com>
2026-06-30 23:10:25 -0600

    baseline wireguard config, maybe

---
+++ modules/features/korimer/wireguard.nix
@@ -0,0 +1,31 @@
+{
+  den.aspects.korimer.provides.wireguard = {
+    nixos = { config, ... }:
+    let
+      Secret = name:
+        config.age.secrets."${name}.age".path;
+    in
+    {
+      networking.wg-quick = {
+        interfaces = {
+          wg-uni = {
+            # TODO: add secret key
+            #privateKey = Secret "wg-edu-key";
+            address = [ "192.168.100.40/32" ];
+            dns = [ "129.82.233.44" ];
+            peers = [{
+              publicKey = "129.82.233.44";
+              allowedIPs = [
+                "129.82.0.0/16"
+                "10.1.0.0/16"
+                "10.2.0.0/16"
+              ];
+              endpoint = "129.82.233.44:52813";
+              persistentKeepalive = 30;
+            }];
+          };
+        };
+      };
+    };
+  };
+}
