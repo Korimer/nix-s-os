0afaf7266a95809f30104a72273ef5d4e36b9db8
Korimer <thekclov@gmail.com>
2026-07-03 17:56:41 -0600

    working, CLEAN secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,48 +1,50 @@
 let
-  pkgs = import (builtins.getFlake "nixpkgs") {};
-  lib = pkgs.lib;
-
   hosts = {
     asya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM2Ayztm9Ld1IQb7JHsN3ASmpHtZHJOEDzK3utkBCEg7";
     magic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKvepbbA6/tH0NzokUcOccn9+dePRH5oy1+XXPKgIDlv";
     #vorkuta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtD3dFec6xPv5Dspc+cAo/EBb135q5reLZQ0u+T7lOa";
   };
 
+  ownership = (with hosts; {
+    nixflix = [ asya ];
+    csu-vpn = [ magic ];
+  });
+
+  ##############################
+  ##### 400 lines of logic #####
+  ##############################
+
+  pkgs = import (builtins.getFlake "nixpkgs") {};
+  lib = pkgs.lib;
+
   secrets = import ./_secretList.nix;
 
-  MapKeysTo = name: owners: lib.genAttrs
-    secrets.${name}
-    (_: { publicKeys = owners; })
-  ;
-  
-  MergeAll = secret: lib.zipAttrsWith
-    (_: owners: 
-      lib.unique (lib.concatLists (
-        map
-          (owner: owner.publicKeys)
-          owners
-      ))
+  GenPair = group: file: lib.nameValuePair
+    file ownership.${group};
+
+  GenerateFileOwnerPairs = group: lib.foldl'
+    (acc: x:
+      acc ++ [(GenPair group x)]
     )
-    secret
+    []
+    secrets.${group}
   ;
 
-  AsSecrets = ownership:
-  let
-    grouped = map
-      (group: MapKeysTo group ownership.${group})
-      (builtins.attrNames ownership);
-  in
-    MergeAll grouped
+  allPairs = lib.flatten (map
+    GenerateFileOwnerPairs
+    (builtins.attrNames ownership)
+  );
+
+  allSecrets = lib.foldl'
+    (acc: x: let
+      cur_keys = acc.${x.name} or [];
+    in 
+    acc //
+    {
+      ${x.name}.publicKeys = lib.unique (cur_keys ++ x.value);
+    })
+    {}
+    allPairs
   ;
-    
 in
-AsSecrets
-(with hosts; {
-  nixflix = [ asya ];
-  csu-vpn = [ magic ];
-})
-#MapKeysTo "nixflix" [ hosts.asya ]
-  #"armored-secret.age" = {
-  #  publicKeys = [ vorkuta ];
-  #  armor = true;
-  #};
+allSecrets
