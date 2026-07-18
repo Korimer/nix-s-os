c54485611aec1eca30dd2c7abb7a0ab843f076f6
Korimer <thekclov@gmail.com>
2026-07-18 00:51:27 -0600

    bittorrent password

--- modules/users/asya/nixflix/qbittorrent.nix
+++ modules/users/asya/nixflix/qbittorrent.nix
@@ -1,10 +1,11 @@
 {
 
-  den.aspects.asya.provides.nixflix.provides.qbittorrent.nixos =
+  den.aspects.asya.provides.nixflix.provides.qbittorrent.nixos = { config, ... }:
   {
     nixflix.torrentClients.qbittorrent = {
       enable = true;
       vpn.enable = true;
+      password._secret = config.age.secrets.jellyfin_pw_bittorrent.path;
     };
   };
 }
--- secrets/_secretList.nix
+++ secrets/_secretList.nix
@@ -18,6 +18,7 @@ rec {
       "tosya_pw"
       "marina_pw"
       "jellyfin_wg_config"
+      "jellyfin_pw_bittorrent"
       "jellyfin_pw_sonarr"
       "jellyfin_pw_radarr"
       "jellyfin_pw_lidarr"
