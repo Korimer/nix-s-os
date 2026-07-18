51ebd4e4180fd59277971eb6577ed0a82bf3d863
Korimer <thekclov@gmail.com>
2026-07-18 00:17:38 -0600

    adding ssh key declaratively

---
+++ modules/users/asya/ssh.nix
@@ -0,0 +1,9 @@
+{
+  den.aspects.asya.provides.ssh-keys = { user, ... }: {
+    nixos = {
+      users.users.${user.userName}.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOieZglZHHs3q+/fO9NZ3b7zfvgscb6q8aaIdAOuIaxu asya@192.168.10.92"
+      ];
+    };
+  };
+}
