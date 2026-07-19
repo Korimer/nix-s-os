da33604e0ff70b93550e9088a8487638d194fbfc
Korimer <thekclov@gmail.com>
2026-07-19 14:46:04 -0600

    add sshkey for laptop

--- modules/users/asya/ssh.nix
+++ modules/users/asya/ssh.nix
@@ -3,6 +3,7 @@
     nixos = {
       users.users.${user.userName}.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOieZglZHHs3q+/fO9NZ3b7zfvgscb6q8aaIdAOuIaxu asya@192.168.10.92"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILaN75fMg5bRLVzT7mFLip9rIpZh5oDVZjua8ZNvTmgk korimer@magic"
       ];
     };
   };
