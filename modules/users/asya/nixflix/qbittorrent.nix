aa3fb526a17752e701ab7c56e421554d9289b06f
Korimer <thekclov@gmail.com>
2026-07-21 01:22:16 -0600

    new qbittorrent pw attempt, applied

--- modules/users/asya/nixflix/qbittorrent.nix
+++ modules/users/asya/nixflix/qbittorrent.nix
@@ -1,14 +1,33 @@
 {
-  den.aspects.asya.provides.nixflix.provides.qbittorrent.nixos = { config, ... }:
+  den.aspects.asya.provides.nixflix.provides.qbittorrent.nixos = { config, lib, pkgs, ... }:
   {
+
+    services.qbittorrent.serverConfig = {
+      Preferences.WebUI = {
+        Username = "admin";
+        Password_PBKDF2 = "REPLACEME";
+      };
+    };
+    
+    systemd.services.qbittorrent.serviceConfig.ExecStartPre = lib.mkBefore ''
+      cfg="${config.services.qbittorrent.profileDir}/qBittorrent/config/qBittorrent.conf"
+
+      ${pkgs.gnused}/bin/sed -i \
+        "s|^Preferences\\\\WebUI\\\\Password_PBKDF2=.*|Preferences\\\\WebUI\\\\Password_PBKDF2=$(cat ${config.age.secrets.jellyfin_pw_bittorrent_webui.path})|" \
+        "$cfg"
+    '';
+
     nixflix.torrentClients.qbittorrent = {
       enable = true;
       vpn.enable = true;
+
       password._secret =
         config.age.secrets.jellyfin_pw_bittorrent_webui.path;
 
-      serverConfig.Preferences.WebUI.Password_PBKDF2 =
-        config.age.secrets.jellyfin_pw_bittorrent_webui.path;
+      serverConfig.Preferences.WebUI = {
+        Username = "admin";
+        Password_PBKDF2 = "REPLACEME";
+      };
     };
   };
 }
