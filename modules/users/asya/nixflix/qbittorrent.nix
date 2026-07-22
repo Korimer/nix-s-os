c0511303bacdc1ad045c4d1beba58420c8395566
Korimer <thekclov@gmail.com>
2026-07-21 22:18:43 -0600

    update bitwarden pw

--- modules/users/asya/nixflix/qbittorrent.nix
+++ modules/users/asya/nixflix/qbittorrent.nix
@@ -1,28 +1,31 @@
 {
   den.aspects.asya.provides.nixflix.provides.qbittorrent.nixos = { config, lib, pkgs, ... }:
+
   {
+    systemd.services.qbittorrent.serviceConfig.LoadCredential =
+      "bittorrent_webui_password:${config.age.secrets.jellyfin_pw_bittorrent_hashed.path}";
 
-    services.qbittorrent.serverConfig = {
-      Preferences.WebUI = {
-        Username = "admin";
-        Password_PBKDF2 = "REPLACEME";
-      };
-    };
-    
-    systemd.services.qbittorrent.serviceConfig.ExecStartPre = lib.mkBefore ''
-      cfg="${config.services.qbittorrent.profileDir}/qBittorrent/config/qBittorrent.conf"
+    systemd.services.qbittorrent.serviceConfig.ExecStartPre = lib.mkAfter [
+      (pkgs.writeShellScript "qbittorrent-patch-password" ''
+        set -euo pipefail
 
-      ${pkgs.gnused}/bin/sed -i \
-        "s|^Preferences\\\\WebUI\\\\Password_PBKDF2=.*|Preferences\\\\WebUI\\\\Password_PBKDF2=$(cat ${config.age.secrets.jellyfin_pw_bittorrent_webui.path})|" \
-        "$cfg"
-    '';
+        config_file="${config.services.qbittorrent.profileDir}/qBittorrent/config/qBittorrent.conf"
+
+        password_hash="$(${pkgs.coreutils}/bin/cat \
+          "$CREDENTIALS_DIRECTORY/bittorrent_webui_password")"
+
+        ${pkgs.gnused}/bin/sed -i \
+          "s|^WebUI\\\\Password_PBKDF2=.*|WebUI\\\\Password_PBKDF2=$password_hash|" \
+          "$config_file"
+      '')
+    ];
 
     nixflix.torrentClients.qbittorrent = {
       enable = true;
       vpn.enable = true;
 
       password._secret =
-        config.age.secrets.jellyfin_pw_bittorrent_webui.path;
+        config.age.secrets.jellyfin_pw_bittorrent.path;
 
       serverConfig.Preferences.WebUI = {
         Username = "admin";
