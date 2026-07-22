69ec99ebcd269b6d093ef215fe5b1ffc5754eacc
Korimer <thekclov@gmail.com>
2026-07-22 16:33:27 -0600

    fix librewolf policy syntax

--- modules/users/korimer/librewolf.nix
+++ modules/users/korimer/librewolf.nix
@@ -14,17 +14,17 @@
             "ExtensionSettings": {
               "uBlock0@raymondhill.net": {
                 "install_url": "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi",
-                "installation_mode": "force_installed"
+                "installation_mode": "force_installed",
                 "default_area": "navbar"
               },
               "{446900e4-71c2-419f-a6a7-df9c091e268b}": {
                 "install_url": "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi",
-                "installation_mode": "force_installed"
+                "installation_mode": "force_installed",
                 "default_area": "navbar"
               },
               "vim-vixen@i-beam.org": {
                 "install_url": "https://addons.mozilla.org/firefox/downloads/latest/vim-vixen/latest.xpi",
-                "installation_mode": "force_installed"
+                "installation_mode": "force_installed",
                 "default_area": "navbar"
               }
             }
