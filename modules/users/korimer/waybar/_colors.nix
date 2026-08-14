94d414d94e2ae8cf1d05db19d38cf38165821bf2
Korimer <thekclov@gmail.com>
2026-08-13 20:36:12 -0600

    crud removed colors

---
+++ modules/users/korimer/waybar/_colors.nix
@@ -0,0 +1,11 @@
+{
+  blue = "#41a6b5";
+  cyan = "#1abc9c";
+  foreground = "#c0caf5";
+  green = "#9ece6a";
+  magenta = "#ff007c";
+  orange = "#ff9e64";
+  purple = "#bb9af7";
+  red = "#f7768e";
+  yellow = "#e0af68";
+}
