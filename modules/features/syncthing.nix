f7f7db917a2df9e2fdba73da27976f02ee06c91c
Korimer <thekclov@gmail.com>
2026-09-08 21:23:29 -0600

    syncthang

--- modules/features/syncthing.nix
+++ modules/features/syncthing.nix
@@ -1,9 +1,10 @@
 {
   den.aspects.syncthing.nixos = { config, ... }: {
+    my.secretGroups = [ "syncthing" ];
     services.syncthing = {
       enable = true;
       openDefaultPorts = true;
-      guiPasswordFile = config.age.secrets.agenix_pw.path;
+      guiPasswordFile = config.age.secrets.syncthing_pw.path;
       settings.gui.user = "admin";
       devices = {
         "netzach" = { id = "YIYS5YI-AFSTCSS-TKLY2O7-WROAXZG-VFGXWEU-QRA5YPL-Z4YUALG-GM4OJQQ"; };
