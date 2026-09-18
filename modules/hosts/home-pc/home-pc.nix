1166351ef5349692e8b8e59388fd2832f47f2318
Korimer <thekclov@gmail.com>
2026-09-18 16:09:55 -0600

    base binds or smth

--- modules/hosts/home-pc/home-pc.nix
+++ modules/hosts/home-pc/home-pc.nix
@@ -8,18 +8,19 @@
     includes = [
       den.aspects.nvidia
       den.aspects.binfmt.provides.aarch64-linux
-      den.aspects.netzach.provides.nirimonitors
+      den.aspects.netzach.provides.monitors
     ];
 
-    environment.etc."mango/specializations.conf".text = ''
-      monitorrule=model:209MXUNBQ390,transform:1,x:0,y:0
-      monitorrule=model:UWZ190700593,transform:0,x:1080,y:280
-    '';
 
-    provides.nirimonitors = {
+    provides.monitors = {
       includes = [ den.aspects.niriconfig ];
 
       nixos = {
+        environment.etc."mango/specializations.conf".text = ''
+          monitorrule=model:209MXUNBQ390,transform:1,height:1920,width:1080,x:0,y:0
+          monitorrule=model:UWZ190700593,transform:0,height:1440,width:2560,x:1080,y:280
+        '';
+
         niriconfig.monitors.text = ''
           output "DP-3" {
             position x=1080 y=356
